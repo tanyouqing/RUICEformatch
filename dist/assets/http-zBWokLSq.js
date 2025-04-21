b12665d034cf3ca995cc382110cac8f15986e327
tanyouqing <1632777869@qq.com>
2025-04-21 23:14:49 +0800

    final(dist)

---
+++ dist/assets/http-zBWokLSq.js
@@ -0,0 +1,2 @@
+import{a as t}from"./axios-CCb-kr4I.js";const r=t.create({baseURL:"http://47.239.17.192:80/",headers:{"ngrok-skip-browser-warning":"true"},timeout:4e5});export{r as h};
+//# sourceMappingURL=http-zBWokLSq.js.map
