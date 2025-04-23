a96da5b1892e18cdd17fe626d8eaded0f669421e
tanyouqing <1632777869@qq.com>
2025-04-23 16:02:24 +0800

    second final(dist)

---
+++ dist/assets/useToast-CWDGN--A.js
@@ -0,0 +1,2 @@
+import{j as a,U as l,I as r,W as u,X as c,Y as i}from"./index-By11hEvf.js";const p=()=>{const e=r();return a(()=>{var o;return((o=l())==null?void 0:o._context)||(e==null?void 0:e.appContext)})},v=()=>{const e=p(),o=[],s=t=>{const n=u(t,e.value);return n&&o.push(n),n};return{init:t=>s(t),notify:s,close:t=>c(t),closeAll:(t=!1)=>i(t?void 0:e.value),closeAllCreatedInThisHook:()=>{o.forEach(t=>c(t))}}};export{v as u};
+//# sourceMappingURL=useToast-CWDGN--A.js.map
